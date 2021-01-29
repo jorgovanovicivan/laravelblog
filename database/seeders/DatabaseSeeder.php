7e62da6385fb89e63bbb20e1dc3b28c8ae361529
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2021-01-29 17:28:16 +0100

    izmene seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(PostTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
